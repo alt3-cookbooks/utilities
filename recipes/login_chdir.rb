3ad752b4c1a9f8a7713c63f7f145c8b63b556eb7
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-10-11 13:57:27 +0200

    Omnipotents login_chdir

--- recipes/login_chdir.rb
+++ recipes/login_chdir.rb
@@ -8,5 +8,6 @@ ruby_block "Set login directory" do
     file = Chef::Util::FileEdit.new("/home/#{node.utilities[:login_chdir][:user]}/#{node.utilities[:login_chdir][:startup_file]}")
     file.insert_line_if_no_match(/cd #{node.utilities[:login_chdir][:directory]}/, "cd #{node.utilities[:login_chdir][:directory]}")
     file.write_file
+    not_if "grep -q 'cd #{node.utilities[:login_chdir][:directory]}' /home/#{node.utilities[:login_chdir][:user]}/#{node.utilities[:login_chdir][:startup_file]}"
   end
 end
