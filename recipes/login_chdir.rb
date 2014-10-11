b2a1cc319a533d6776014b30ff5978264c7c17aa
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-10-11 14:15:15 +0200

    Refactors login_chdir

--- recipes/login_chdir.rb
+++ recipes/login_chdir.rb
@@ -5,9 +5,9 @@
 
 ruby_block "Set login directory" do
   block do
-    file = Chef::Util::FileEdit.new("/home/#{node.utilities[:login_chdir][:user]}/#{node.utilities[:login_chdir][:startup_file]}")
-    file.insert_line_if_no_match(/cd #{node.utilities[:login_chdir][:directory]}/, "cd #{node.utilities[:login_chdir][:directory]}")
-    file.write_file
-    not_if "grep -q 'cd #{node.utilities[:login_chdir][:directory]}' /home/#{node.utilities[:login_chdir][:user]}/#{node.utilities[:login_chdir][:startup_file]}"
+    File.open(Chef::Util::FileEdit.new("/home/#{node.utilities[:login_chdir][:user]}/#{node.utilities[:login_chdir][:startup_file]}"), 'a') do |file|
+      file.puts "\ncd #{node.utilities[:login_chdir][:directory]}"
+    end
   end
+  not_if "grep -q 'cd #{node.utilities[:login_chdir][:directory]}' /home/#{node.utilities[:login_chdir][:user]}/#{node.utilities[:login_chdir][:startup_file]}"
 end
