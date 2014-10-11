6b51d706b7d8d62e5fa072c64ba2f4483d510fa0
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-10-11 14:38:17 +0200

    Bumps version

--- metadata.rb
+++ metadata.rb
@@ -2,5 +2,5 @@ name             'utilities'
 maintainer       'alt3.io'
 license          'MIT'
 description      'Various convenience recipes.'
-version          '0.1'
+version          '1.0.0'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
